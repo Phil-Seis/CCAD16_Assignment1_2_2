8c4cf6fdc476acadac684e87ee1b9c69cdf71c7c
Philip Seisman <Philip.seisman@gmail.com>
2024-11-05 14:01:37 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CCAD16_Assignment1_2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a C# program to find the sum of first 10 natural numbers (1-10):
+            //Use a For loop here
+            int sum = 0;
+            
+            for (int i = 1; i < 11; i++)
+            {
+                sum += i;
+            }
+        Console.WriteLine("The sum of the first 10 natural numbers equals " + sum);
+        }
+    }
+}
